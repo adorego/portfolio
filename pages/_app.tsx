b948f3ac0fd89117b039d33061345c56bb11f11c
Andres Do Rego Barros <adoregoel@gmail.com>
2022-11-07 10:45:18 -0300

    Google Analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,14 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-Q9WZS8QJW0`} />
 
     <Script id={'1'} strategy="lazyOnload">
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    gtag('config', 'G-Q9WZS8QJW0', {
                     page_path: window.location.pathname,
                     });
                 `}
